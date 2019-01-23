a65467d1a0d436cf3aa582b93186849a4a09f04e
Walter Goins <wgoinz@gmail.com>
2019-01-23 12:47:46 -0500

    setup connection with components

---
+++ client/src/components/Creatures.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Creatures extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Creature</h1>
+            </div>
+        );
+    }
+}
+
+export default Creatures;
